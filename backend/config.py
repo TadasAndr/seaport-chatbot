7fde2206f98535fbb0ebd4606e3d71ff56bce3d4
Tadas Andrijauskas <tadasandrijauskas@outlook.com>
2024-09-19 14:06:52 +0300

    fix 7

--- backend/config.py
+++ backend/config.py
@@ -1,16 +1,6 @@
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 
-load_dotenv()
-
-def streamlit_secrets_settings(settings):
-    try:
-        import streamlit as st
-        return st.secrets
-    except ImportError:
-        # Not running in Streamlit, return empty dict
-        return {}
-
 class Settings(BaseSettings):
     OPENAI_API_KEY: str
     PINECONE_API_KEY: str
@@ -20,13 +10,5 @@ class Settings(BaseSettings):
         env_file = ".env"
         env_file_encoding = "utf-8"
 
-        @classmethod
-        def customise_sources(cls, init_settings, env_settings, file_secret_settings):
-            return (
-                init_settings,
-                streamlit_secrets_settings,
-                env_settings,
-                file_secret_settings,
-            )
 
 config = Settings()
